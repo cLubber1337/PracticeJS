d3472242adb94578e285e363e6a5b3a180081adb
Andrey <supersoroko@gmail.com>
2022-11-01 00:40:43 +0300

    Task#1

--- external.js
+++ external.js
@@ -1,5 +1,24 @@
-"use strict";
+// Задание№1. Консольная программа
 
-const isChecked = false;
-const isClose = false;
-console.log(isChecked || isClose);
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let lastWachedfilms = prompt("Один из последних просмотренных фильмов?", "");
+
+let filmScore = prompt("На сколько оцените его?", "");
+
+let lastWachedfilms2 = prompt("Один из последних просмотренных фильмов?", "");
+
+let filmScore2 = prompt("На сколько оцените его?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.movies[lastWachedfilms] = filmScore;
+personalMovieDB.movies[lastWachedfilms2] = filmScore2;
+
+console.log(personalMovieDB);
